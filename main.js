9a0addc0c16762299a51868989560a608532a599
Marco Rossignoli <marcorossignoli@pop-os.localdomain>
2021-01-17 16:00:01 +0100

    :sparkles: first functional build

--- main.js
+++ main.js
@@ -10,17 +10,26 @@ let hof_channel;
 
 client.on('ready', () => {
     console.log('I am ready!');
-    hof_channel = client.channels.cache.get('799957406058151986');
-
-
+    hof_channel = client.channels.cache.get('789977985154678785');
     deleteMessages();
+    countMedals()
+    
 });
 
 client.on('message', message => {
-    if(message.channel === hof_channel){
-        if(message.content.startsWith('@')){
+    if(message.author == client.user) return;
 
-        }
+    if(message.channel === hof_channel){
+        deleteMessages();
+        countMedals();
+    }
+});
+
+client.on("messageDelete", (msgDelete)=>{
+    if(msgDelete.author == client.user) return;
+    if(msgDelete.channel === hof_channel){
+        deleteMessages();
+        countMedals();
     }
 });
 
@@ -35,23 +44,57 @@ function deleteMessages(){
 
 function countMedals(){
     let users = {};
-    let messages = hof_channel.messages.fetch();
-    messages.forEach(msg => {
-        let usr = msg.mentions.users[0];
-        users[usr] = users[usr] === 'undefined' ? 0 : users[usr]++;
-    });
+    hof_channel.messages.fetch()
+    .then(messages =>{
+        messages.forEach(msg => {
+            if(msg.mentions.users.size > 0){
+                let usr = msg.mentions.users.first().username;
+                if(users.hasOwnProperty(usr)){
+                    users[usr]++;
+                } else {
+                    users[usr] = 1;
+                }
+            }
+            
+        });
 
+        createMessage(users);
+    });
     return users;
 }
 
 function createMessage(users){
-    var sortable = [];
-    users.forEach(usr =>{
-        sortable.push()
-    });
+    let orderedUsers = sortObject(users);
+    let text = '\n';
+    const keys = Object.keys(orderedUsers);
+    
+    for(i = 0; i < keys.length; i++){
+        switch(i){
+            case 0:
+                text += '\n:first_place:' + keys[i] + ' : ' + orderedUsers[keys[i]] + "\n"; 
+                break;
+            case 1:
+                text += ':second_place:' + keys[i]+ ' : ' + orderedUsers[keys[i]] + "\n"; 
+                break;
+            case 2:
+                text += ':third_place:' + keys[i] + ' : ' + orderedUsers[keys[i]] + "\n"; 
+                break;
+            default:
+                text += ':regional_indicator_l:' + keys[i] + ' : ' + orderedUsers[keys[i]] + "\n"; 
+                break;
+        }
+    }
+
+    hof_channel.send(text);
 }
 
+function sortObject(obj){
+    const sortable = Object.fromEntries(
+        Object.entries(obj).sort(([,a],[,b]) => b-a)
+    );
 
+    return sortable;
+}
 
 
 // Log our bot in using the token from https://discord.com/developers/applications
